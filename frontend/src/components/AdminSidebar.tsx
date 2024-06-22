8cb50b1e87628f6d7c0b3df1d429538f10a4de76
CodeWithSomesh <codewithsomesh@gmail.com>
2024-06-22 12:56:34 +0800

    Added Exit Admin button that redirects users to the Customer Home Page

--- frontend/src/components/AdminSidebar.tsx
+++ frontend/src/components/AdminSidebar.tsx
@@ -1,8 +1,11 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import Divider from '@mui/material/Divider';
-import { BadgePlus, CircleDollarSign, Home, LogOut, ShoppingBag } from 'lucide-react';
+import { BadgePlus, CircleArrowOutUpRight, CircleDollarSign, Home, LogOut, ShoppingBag } from 'lucide-react';
 
 
 export default function AdminSidebar() {
+
+  const { logout } = useAuth0();
 
   // const { createShop, isLoading: isCreateLoading } = useCreateMyShop();
   // const { shop } = useGetMyShop();
@@ -11,17 +14,16 @@ export default function AdminSidebar() {
   // const isEditing = !!shop;
 
   return (
-    <div className=''>
-      <div className='w-[350px] border fixed top-0 left-0 h-full'>
+    <div className='relative'>
+      <div className='w-[350px] border fixed top-0 left-0 h-full flex flex-col'>
         <h1 className="text-3xl font-bold tracking-tight font-inter text-center my-4">
           SoRa Sneakers
         </h1>
 
         <Divider />
 
-        <div className=''>
-
-          <div className='flex justify-center items-center my-4 gap-3  mx-6 py-4 rounded-md hover:bg-violet2 hover:text-white '>
+        <div className='flex-grow overflow-auto'>
+          <div className='flex justify-center items-center my-4 gap-3 mx-6 py-4 rounded-md hover:bg-violet2 hover:text-white '>
             <Home width={36} height={36} />
             <p className='text-2xl font-inter font-semibold '>Home Page Details</p>
           </div>
@@ -31,25 +33,29 @@ export default function AdminSidebar() {
             <p className='text-2xl font-inter font-semibold '>Add Product</p>
           </div>
 
-          <div className='flex justify-center items-center my-4 gap-3  mx-6 py-4 rounded-md hover:bg-violet2 hover:text-white '>
+          <div className='flex justify-center items-center my-4 gap-3 mx-6 py-4 rounded-md hover:bg-violet2 hover:text-white '>
             <ShoppingBag width={36} height={36} className='-ml-[38px]' />
             <p className='text-2xl font-inter font-semibold '>Product Details</p>
           </div>
 
-          <div className='flex justify-center items-center my-4 gap-3  mx-6 py-4 rounded-md hover:bg-violet2 hover:text-white '>
+          <div className='flex justify-center items-center my-4 gap-3 mx-6 py-4 rounded-md hover:bg-violet2 hover:text-white '>
             <CircleDollarSign width={36} height={36} className='-ml-[63px]' />
             <p className='text-2xl font-inter font-semibold '>Order Details</p>
           </div>
-
-          <div className='mt-[235px] border'></div>
-
-          <div className='flex justify-center items-center mt-4 mb-4 gap-3  mx-6 py-4 rounded-md hover:bg-violet2 hover:text-white'>
-            <LogOut width={36} height={36} className='-ml-[140px]' />
-            <p className='text-2xl font-inter font-semibold '>Logout</p>
-          </div>
-
         </div>
-      
+
+        <div className='border w-full my-4'></div>
+
+        <div onClick={() => logout()} className='flex justify-center items-center mx-6 gap-4 py-4 my-4 rounded-md hover:bg-violet2 hover:text-white'>
+          <CircleArrowOutUpRight width={30} height={30} className='-ml-[82px]' />
+          <p className='text-2xl font-inter font-semibold '>Exit Admin</p>
+          
+        </div>
+
+        <div className='flex justify-center items-center mb-4 mx-6 gap-3 py-4 rounded-md hover:bg-violet2 hover:text-white'>
+          <LogOut width={36} height={36} className='-ml-[120px]' />
+          <p className='text-2xl font-inter font-semibold '>Logout</p>
+        </div>
       </div>
 
       

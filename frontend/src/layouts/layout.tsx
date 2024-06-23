a7d4362157619b18afff6c35b1f009c7eac6f830
CodeWithSomesh <codewithsomesh@gmail.com>
2024-06-24 03:40:40 +0800

    Finally completed the Filter By Product Tag feature. Users can now filter by "On Sale", "New Arrivals", and also "Best Selling" products

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,12 +1,14 @@
 import AsSeenOnSection from "@/components/AsSeenOnSection";
 import BannerCategory from "@/components/BannerCategory";
 import FAQ from "@/components/FAQ";
+import FilteringDropdown from "@/components/FilteringDropdown";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import NavbarPromoMessage from "@/components/NavbarPromoMessage";
 import NewArrivals from "@/components/NewArrivals";
 import { EmblaOptionsType } from 'embla-carousel'
+import { useLocation } from "react-router-dom";
 
 //Declaring Typescript types for variables
 type Props = {
@@ -20,6 +22,9 @@ const SLIDE_COUNT = 8
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 const Layout = ({children, homePage = false, adminPage = false,}: Props) => {
+    const location = useLocation();
+    const path = location.pathname
+
     return (
         <div className="flex flex-col min-h-screen">
             
@@ -33,7 +38,8 @@ const Layout = ({children, homePage = false, adminPage = false,}: Props) => {
                     {homePage &&  <NewArrivals slides={SLIDES} options={OPTIONS} />} 
                     {homePage &&  <BannerCategory />} 
                     {/* {homePage &&  <AsSeenOnSection />}  */}
-                    <div className="container mx-auto flex-1 py-10">{children}</div>
+
+                    <div >{children}</div>
                     {homePage &&  <FAQ />} 
                     <Footer />
                 </div>
--- frontend/src/pages/MenProductPage.tsx
+++ frontend/src/pages/MenProductPage.tsx
@@ -2,9 +2,10 @@ import { useGetProduct } from '@/api/ProductsApi';
 import { Product } from '@/types';
 import Counter from '@/components/Counter';
 import { useState } from 'react';
+import FilteringDropdown from '@/components/FilteringDropdown';
 
 
-
+type FilterTerm = string | undefined;
 
 const MenProductPage: React.FC = () => {
   const { product } = useGetProduct(); //Retrieving All products from the DB 
@@ -12,6 +13,9 @@ const MenProductPage: React.FC = () => {
 
   // State to track the hover state for each product
   const [hoveredProduct, setHoveredProduct] = useState<{ [key: string]: boolean }>({});
+  const [filterTerm, setFilterTerm] = useState<FilterTerm>(undefined);
+
+  
 
   const transformTag = (tag:string) => {
     switch (tag) {
@@ -29,53 +33,138 @@ const MenProductPage: React.FC = () => {
   // Filter products to only include those with productCategory "men"
   const filteredProducts = Array.isArray(product) ? product.filter(shoe => shoe.productCategory === 'men') : [];
 
+  // Function to filter products by product tag
+  const filterProductsByTag = (products: Product[], tag: string): Product[] => {
+    if (!products) return [];
+
+    return products.filter(product => product.productTags === tag);
+  };
+
+  let newlyFilteredProducts: Product[] = []
+
+  if (filterTerm === "bestSelling") {
+    newlyFilteredProducts = filterProductsByTag(filteredProducts, filterTerm)
+  } else if (filterTerm === "newArrivals") {
+    newlyFilteredProducts = filterProductsByTag(filteredProducts, filterTerm)
+  } else if (filterTerm === "onSale"){
+    filteredProducts.filter(product => {
+      const offer =  parseInt(product.productTags, 10)
+      console.log(offer) 
+
+      if (offer){
+        newlyFilteredProducts = filteredProducts.filter(product => product.productTags === offer.toString()) 
+      }
+      
+    })
+  }else {
+
+    newlyFilteredProducts = filteredProducts
+  }
+
+
+
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols- gap-6'>
-        {filteredProducts.map((shoe: Product) => {
+    <div>
+      <FilteringDropdown filterTerm={filterTerm} setFilterTerm={setFilterTerm} />
+
+      <div className="container mx-auto flex-1 py-10">
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols- gap-6 '>
+          {!filterTerm && filteredProducts.map((shoe: Product) => {
             const newTag = transformTag(shoe.productTags)
             const offer  = typeof newTag === "string" ? parseInt(newTag, 10) : 0;
-            console.log(offer)
+
 
             return (
-                <div>
-                    <div key={shoe.productCode} className=' flex flex-col items-center rounded-lg border h-full border-gray-400 shadow-lg transition-all hover:shadow-2xl hover:scale-105'>
-                        <div  className='border-b w-full items-center justify-center flex rounded-sm border-gray-300 relative' 
-                        onMouseEnter={() => setHoveredProduct(prevState => ({ ...prevState, [shoe.productCode]: true }))} 
-                        onMouseLeave={() => setHoveredProduct(prevState => ({ ...prevState, [shoe.productCode]: false }))}>
-                            <img 
-                            src={`http://localhost:7000/uploads/${hoveredProduct[shoe.productCode] ? shoe.productImages[1] : shoe.productImages[0]}`}
-                            height={20} width={320} alt="" className='' />
+              <div>
+                <div key={shoe.productCode} className=' flex flex-col items-center rounded-lg border h-full border-gray-400 shadow-lg transition-all hover:shadow-2xl hover:scale-105'>
+                  <div  className='border-b w-full items-center justify-center flex rounded-sm border-gray-300 relative' 
+                    onMouseEnter={() => setHoveredProduct(prevState => ({ ...prevState, [shoe.productCode]: true }))} 
+                    onMouseLeave={() => setHoveredProduct(prevState => ({ ...prevState, [shoe.productCode]: false }))}>
+                        <img 
+                        src={`http://localhost:7000/uploads/${hoveredProduct[shoe.productCode] ? shoe.productImages[1] : shoe.productImages[0]}`}
+                        height={20} width={320} alt="" className='' />
 
-                            <p className={`absolute italic top-4 right-4 text-nowrap h-fit font-semibold text-[18px] rounded-full text-white px-6 py-2 text-green-60 ${newTag ? "bg-[#836FFF]" : ""} `}>{newTag}</p>
+                        <p className={`absolute italic top-4 right-4 text-nowrap h-fit font-semibold text-[18px] rounded-full text-white px-6 py-2 text-green-60 ${newTag ? "bg-[#836FFF]" : ""} `}>{newTag}</p>
+                  </div>
+
+                  <div className='flex flex-col h-full  w-full px-4 font-inter gap-10 pt-4 pb-5 '>
+                    <h1 className='text-[18px] font-bold min-h-[50px]'>{(shoe.productName).toUpperCase()}</h1>
+
+                    <div className='flex justify-between h-full items-end'>
+                      {offer > 0 ? (
+                        <div className=''>
+                          <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full  px-6 py- text-center line-through italic'>RM {shoe.productPrice}</p>
+                          <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full bg-green-100 px-6 py-2 text-green-60'>RM {(Number(shoe.productPrice) * offer/100 ).toFixed(2)}</p>
+                            
                         </div>
-
-                        <div className='flex flex-col h-full  w-full px-4 font-inter gap-10 pt-4 pb-5 '>
-                          <h1 className='text-[18px] font-bold min-h-[50px]'>{shoe.productName}</h1>
-
-                          <div className='flex justify-between h-full items-end'>
-                            {offer > 0 ? (
-                              <div className=''>
-                                <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full  px-6 py- text-center line-through'>RM {shoe.productPrice}</p>
-                                <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full bg-green-100 px-6 py-2 text-green-60'>RM {(Number(shoe.productPrice) * offer/100 ).toFixed(2)}</p>
-                                  
-                              </div>
-                            )
-                            : (
-                              <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full bg-green-100 px-6 py-2 text-green-60'>RM {shoe.productPrice}</p>
-                            )
-                          }
+                      )
+                      : (
+                        <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full bg-green-100 px-6 py-2 text-green-60'>RM {shoe.productPrice}</p>
+                      )
+                    }
 
 
-                            <Counter />
-                          </div>
-                        </div>
+                      <Counter />
                     </div>
+                  </div>
                 </div>
-            
-        )})}
+              </div>
+              
+          )})}
 
-            
+
+          {filterTerm && newlyFilteredProducts?.map((shoe: Product) => {
+            const newTag = transformTag(shoe.productTags)
+            const offer  = typeof newTag === "string" ? parseInt(newTag, 10) : 0;
+
+
+            return (
+              <div>
+                <div key={shoe.productCode} className=' flex flex-col items-center rounded-lg border h-full border-gray-400 shadow-lg transition-all hover:shadow-2xl hover:scale-105'>
+                  <div  className='border-b w-full items-center justify-center flex rounded-sm border-gray-300 relative' 
+                    onMouseEnter={() => setHoveredProduct(prevState => ({ ...prevState, [shoe.productCode]: true }))} 
+                    onMouseLeave={() => setHoveredProduct(prevState => ({ ...prevState, [shoe.productCode]: false }))}>
+                        <img 
+                        src={`http://localhost:7000/uploads/${hoveredProduct[shoe.productCode] ? shoe.productImages[1] : shoe.productImages[0]}`}
+                        height={20} width={320} alt="" className='' />
+
+                        <p className={`absolute italic top-4 right-4 text-nowrap h-fit font-semibold text-[18px] rounded-full text-white px-6 py-2 text-green-60 ${newTag ? "bg-[#836FFF]" : ""} `}>{newTag}</p>
+                  </div>
+
+                  <div className='flex flex-col h-full  w-full px-4 font-inter gap-10 pt-4 pb-5 '>
+                    <h1 className='text-[18px] font-bold min-h-[50px]'>{(shoe.productName).toUpperCase()}</h1>
+
+                    <div className='flex justify-between h-full items-end'>
+                      {offer > 0 ? (
+                        <div className=''>
+                          <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full  px-6 py- text-center line-through italic'>RM {shoe.productPrice}</p>
+                          <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full bg-green-100 px-6 py-2 text-green-60'>RM {(Number(shoe.productPrice) * offer/100 ).toFixed(2)}</p>
+                            
+                        </div>
+                      )
+                      : (
+                        <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full bg-green-100 px-6 py-2 text-green-60'>RM {shoe.productPrice}</p>
+                      )
+                    }
+
+
+                      <Counter />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+          )})}
+
+                
+        </div>
+
+      </div>
+      
+
+
     </div>
+    
   )
 }
 

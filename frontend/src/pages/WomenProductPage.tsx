5e229881a01ed712ab43594d879495bb46351ce4
Sara <maisarah.majdi01@gmail.com>
2024-06-24 12:07:22 +0800

    retrieving product from DB in WomenProductPage

--- frontend/src/pages/WomenProductPage.tsx
+++ frontend/src/pages/WomenProductPage.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { useGetProduct } from '@/api/ProductsApi';
+import { Product } from '@/types';
+import Counter from '@/components/Counter';
+import { useState } from 'react';
+import FilteringDropdown from '@/components/FilteringDropdown';
+import { Link } from 'react-router-dom';
 
-const WomenProductPage = () => {
-  return (
-    <div className='h-[800px]'>
-        WomenProductPage
-    </div>
-  )
+
+type FilterTerm = string | undefined;
+
+const WomenProductPage: React.FC = () => {
+  const { product } = useGetProduct(); //Retrieving All products from the DB 
+  //console.log(product)
+  
+
+  
 }
 
 export default WomenProductPage

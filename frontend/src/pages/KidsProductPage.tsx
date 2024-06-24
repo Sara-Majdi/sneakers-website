a0f76b7a146b7a87887a53365d5f90fad44bef48
Sara <maisarah.majdi01@gmail.com>
2024-06-24 13:17:41 +0800

    retrieving products from DB in KidsProductPage

--- frontend/src/pages/KidsProductPage.tsx
+++ frontend/src/pages/KidsProductPage.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { useGetProduct } from '@/api/ProductsApi';
+import { Product } from '@/types';
+import Counter from '@/components/Counter';
+import { useState } from 'react';
+import FilteringDropdown from '@/components/FilteringDropdown';
+import { Link } from 'react-router-dom';
 
-const KidsProductPage = () => {
-  return (
-    <div className='h-[800px]'>
-        KidsProductPage
-    </div>
-  )
+
+type FilterTerm = string | undefined;
+
+const KidsProductPage: React.FC = () => {
+  const { product } = useGetProduct(); //Retrieving All products from the DB 
+  //console.log(product)
+  
+  //
+  
+  //
 }
 
 export default KidsProductPage

25f6d71868348fab40af96b1bf719cd3d48026da
Sara <maisarah.majdi01@gmail.com>
2024-06-24 13:28:44 +0800

    complete with dropdown function for filtering product

--- frontend/src/pages/KidsProductPage.tsx
+++ frontend/src/pages/KidsProductPage.tsx
@@ -60,8 +60,58 @@ const KidsProductPage: React.FC = () => {
   }
 
 
-  //
   
+  return (
+    <div>
+      <FilteringDropdown filterTerm={filterTerm} setFilterTerm={setFilterTerm} />
+
+      <div className="container mx-auto flex-1 py-10">
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols- gap-6 '>
+          {!filterTerm && filteredProducts.map((shoe: Product) => {
+            const newTag = transformTag(shoe.productTags)
+            const offer  = typeof newTag === "string" ? parseInt(newTag, 10) : 0;
+
+
+            return (
+              <div>
+                <Link to={`/products/${shoe._id}`}  key={shoe.productCode} className=' flex flex-col items-center rounded-lg border h-full border-gray-400 shadow-lg transition-all hover:shadow-2xl hover:scale-105'>
+                  <div  className='border-b w-full items-center justify-center flex rounded-sm border-gray-300 relative' 
+                    onMouseEnter={() => setHoveredProduct(prevState => ({ ...prevState, [shoe.productCode]: true }))} 
+                    onMouseLeave={() => setHoveredProduct(prevState => ({ ...prevState, [shoe.productCode]: false }))}>
+                        <img 
+                        src={`http://localhost:7000/uploads/${hoveredProduct[shoe.productCode] ? shoe.productImages[1] : shoe.productImages[0]}`}
+                        height={20} width={320} alt="" className='' />
+
+                        <p className={`absolute italic top-4 right-4 text-nowrap h-fit font-semibold text-[18px] rounded-full text-white px-6 py-2 text-green-60 ${newTag ? "bg-[#836FFF]" : ""} `}>{newTag}</p>
+                  </div>
+
+                  <div className='flex flex-col h-full  w-full px-4 font-inter gap-10 pt-4 pb-5 '>
+                    <h1 className='text-[18px] font-bold min-h-[50px]'>{(shoe.productName).toUpperCase()}</h1>
+
+                    <div className='flex justify-between h-full items-end'>
+                      {offer > 0 ? (
+                        <div className=''>
+                          <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full  px-6 py- text-center line-through italic'>RM {shoe.productPrice}</p>
+                          <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full bg-green-100 px-6 py-2 text-green-60'>RM {(Number(shoe.productPrice) * offer/100 ).toFixed(2)}</p>
+                            
+                        </div>
+                      )
+                      : (
+                        <p className='text-nowrap h-fit font-extrabold text-[20px] rounded-full bg-green-100 px-6 py-2 text-green-60'>RM {shoe.productPrice}</p>
+                      )
+                    }
+
+
+                      <Counter />
+                    </div>
+                  </div>
+                </Link>
+              </div>
+              
+          )})}
+
+          //
+          
   //
 }
 
